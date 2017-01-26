6b0644923ef622079315f2f48fecabb4475c3772
HaydenYang <yangxinhust@hotmail.com>
2017-01-26 22:20:32 +0800

    add log support; log file can not in the source file

--- mitmProxy.py
+++ mitmProxy.py
@@ -5,17 +5,27 @@ import tensorflow as tf
 from mitmproxy.script import concurrent
 from mitmproxy import http
 import uuid
-import time
 from PIL import ImageDraw
 from PIL import ImageFont
-from mitmproxy import ctx
+import logging
 
-
+# can not be same,the script will reload again and again
 path="/Users/hsin/Documents/Project/Python/labelFlower/"
+logPath="/Users/hsin/Documents/img.log"
 
-font = ImageFont.truetype(path+"arial.ttf", 15)
+font = ImageFont.truetype(path+"arial.ttf", 14)
+font.color = "black"
+logger = logging.getLogger('myapp')
 
-ctx.log.info("LOG: "+str(time.time())+" STU LOAD BEGIN")
+hdlr = logging.FileHandler(logPath)
+
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO)
+
+
+logger.info("STU LOAD BEGIN")
 with tf.gfile.FastGFile(path+"retrained_graph.pb", 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
@@ -23,9 +33,10 @@ with tf.gfile.FastGFile(path+"retrained_graph.pb", 'rb') as f:
     sess = tf.Session()
     softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
     erFilePath = path+"1.jpg"
-print("LOG: "+str(time.time())+" STU LOAD END")
+logger.info("STU LOAD END")
 
 reqList=[]
+
 @concurrent
 def response(flow):
     if flow.response.headers.get("content-type", "").startswith("image"):
@@ -34,12 +45,12 @@ def response(flow):
         ranNum = uuid.uuid4().time_low
 
         if(imgUrl in reqList):
-            print("LOG: " + str(time.time()) + " STU PROCESS RETURN "+str(ranNum) + " " + imgUrl)
+            logger.info("STU PROCESS RETURN "+str(ranNum) + " " + imgUrl)
             flow.response = http.HTTPResponse.make(404)
             return
         else:
             reqList.append(imgUrl)
-        print("LOG: " + str(time.time()) + " STU PROCESS BEGIN " + str(ranNum) + " " + imgUrl)
+        logger.info("STU PROCESS BEGIN " + str(ranNum) + " " + imgUrl)
         s = io.BytesIO(flow.response.content)
         s2 = io.BytesIO()
 
@@ -54,7 +65,7 @@ def response(flow):
             return
         predictions = sess.run(softmax_tensor, \
                                {'DecodeJpeg/contents:0': image_data})
-        print("LOG: " + str(time.time()) + " STU PROCESS END " + str(ranNum) + " " + imgUrl)
+        logger.info("STU PROCESS END " + str(ranNum) + " " + imgUrl)
         # Sort to show labels of first prediction in order of confidence
         predictions[0].argsort()[-len(predictions[0]):][::-1]
         # Ads
@@ -63,18 +74,18 @@ def response(flow):
         if predictions[0][0] > 0.5:
             img = Image.open(erFilePath)
             draw = ImageDraw.Draw(img)
-            draw.text((0, 0), str(ranNum), font=font, fill="#000000")
+            draw.text((0, 0), str(ranNum), font=font,fill="#000000")
             img.save(s3,"PNG")
             flow.response.content = s3.getvalue()
             # flow.response.content = s.getvalue()
             flow.response.headers["content-type"] = "image/png"
-            print("LOG: " + str(time.time()) + " STU OUT AD " + str(ranNum) + " " + imgUrl)
+            logger.info("STU OUT AD " + str(ranNum) + " " + imgUrl)
         else:
             img = Image.open(s)
             draw = ImageDraw.Draw(img)
-            draw.text((0, 0), str(ranNum), font=font, fill="#000000")
+            draw.text((0, 0), str(ranNum), font=font,fill="#000000")
             img.save(s3,"PNG")
             flow.response.content = s3.getvalue()
             # flow.response.content = s.getvalue()
             flow.response.headers["content-type"] = "image/png"
-            print("LOG: " + str(time.time()) + " STU OUT NO " + str(ranNum) + " " + imgUrl)
+            logger.info("STU OUT NO " + str(ranNum) + " " + imgUrl)
